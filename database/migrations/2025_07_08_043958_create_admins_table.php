7929ebe1109b8e9e60fa458cd3c041bf7d54e550
zanuaraldi <zanuar0101aldisyahputra@gmail.com>
2025-07-10 11:19:59 +0700

    refactor(migration): rescturcture table(admins)

--- database/migrations/2025_07_08_043958_create_admins_table.php
+++ database/migrations/2025_07_08_043958_create_admins_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('admins', function (Blueprint $table) {
             $table->id('admin_id');
-            $table->string('name'. 100);
+            $table->string('name', 100);
             $table->string('email', 150)->unique();
             $table->string('password');
             $table->string('phone', 20);
