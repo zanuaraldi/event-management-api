ed53467e9f4db88911ba57e57157177ddb6029a6
zanuaraldi <zanuar0101aldisyahputra@gmail.com>
2025-07-14 13:45:52 +0700

    feat(controller): Add delete organizer method

--- app/Http/Controllers/OrganizersController.php
+++ app/Http/Controllers/OrganizersController.php
@@ -7,7 +7,32 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class OrganizersController extends BaseController
 {
-    public function index(){
+    public function index()
+    {
         return OrganizersModel::all();
+    }
+
+    public function destroyOrganizer($id)
+    {
+        try {
+            $organizer = OrganizersModel::findOrFail($id);
+
+            if (!$organizer) {
+                return response()->json([
+                    'success' => false,
+                    'massage' => 'Data tidak ditemukan'
+                ], 400);
+            }
+
+            $organizer->delete();
+            return response()->json([
+                'success' => true,
+                'massage' => 'Data berhasil dihapus'
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'massage' => 'Terjadi kesalahan'
+            ], 500);
+        }
     }
 }
