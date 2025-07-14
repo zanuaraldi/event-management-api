16d9809d73443f4065f8d05a847892fe8b0cdf81
zanuaraldi <zanuar0101aldisyahputra@gmail.com>
2025-07-14 13:26:15 +0700

    feat(controller): Add delete user method

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,12 +3,36 @@
 namespace App\Http\Controllers;
 
 use App\Models\UsersModel;
-use App\Models\EventsModel;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class UsersController extends BaseController
 {
-    public function index(){
+    public function index()
+    {
         return UsersModel::all();
+    }
+
+    public function destroyUser($id)
+    {
+        try {
+            $user = UsersModel::findOrFail($id);
+
+            if (!$user) {
+                return response()->json([
+                    'success' => false,
+                    'massage' => 'Data tidak ditemukan'
+                ], 400);
+            }
+
+            $user->delete();
+            return response()->json([
+                'success' => true,
+                'massage' => 'Data berhasil dihapus'
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'massage' => 'Terjadi kesalahan'
+            ], 500);
+        }
     }
 }
