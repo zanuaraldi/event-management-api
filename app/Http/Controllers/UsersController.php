904ff6c324d48a549bab991805f0cddcc98f78ff
zanuaraldi <zanuar0101aldisyahputra@gmail.com>
2025-07-16 14:21:39 +0700

    feat(controller): Add create user method

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\UsersModel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class UsersController extends BaseController
@@ -29,6 +32,44 @@ class UsersController extends BaseController
                 'success' => true,
                 'massage' => 'Data berhasil dihapus'
             ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'massage' => 'Terjadi kesalahan'
+            ], 500);
+        }
+    }
+
+    public function storeUser(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|string|max:100',
+                'email' => 'required|string|max:150|unique:users,email',
+                'password' => 'required|string|max:255',
+                'phone' => 'required|digits_between:11,20',
+                'photo_url' => 'string|max:255'
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'message' => 'Validasi gagal',
+                    'error' => $validator->errors()
+                ], 400);
+            }
+
+            $user = UsersModel::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+                'phone' => $request->phone,
+                'photo_url' => $request->photo_url
+            ]);
+
+            return response()->json([
+                'success' => true,
+                'message' => 'User baru berhasil di buat',
+                'data' => $user
+            ], 201);
         } catch (\Throwable $th) {
             return response()->json([
                 'massage' => 'Terjadi kesalahan'
